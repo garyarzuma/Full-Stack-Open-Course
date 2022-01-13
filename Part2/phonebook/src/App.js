859d59ac639c60573738f174f742400582ce68c9
Gary Arzumanyan <garyarzumanyan@gmail.com>
2022-01-12 22:55:19 -0800

    Finished ex 2.8

--- Part2/phonebook/src/App.js
+++ Part2/phonebook/src/App.js
@@ -2,21 +2,23 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas' , number: "818-229-2234"}
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
-  const addName = (event) => {
+  const addContact = (event) => {
     event.preventDefault()
-    const nameObject = {name: newName}
+    const contactObject = {name: newName, number: newNumber}
     if (persons.map((person) => JSON.stringify(person))  //use Json to compare objects
-                                .includes(JSON.stringify(nameObject))) {
-      //console.log("duplicate name", nameObject) 
+                                .includes(JSON.stringify(contactObject))) {
+      //console.log("duplicate name", contactObject) 
       window.alert(`${newName} is already added to phonebook`)
       return;
     }
-    setPersons(persons.concat(nameObject))
+    setPersons(persons.concat(contactObject))
     setNewName("");
+    setNewNumber("");
   }
 
   
@@ -26,19 +28,28 @@ const App = () => {
     setNewName(event.target.value)
   }
 
+  
+  const handleNumberChange = (event) => {
+    //console.log(event.target.value)
+     setNewNumber(event.target.value)
+   }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addName}>
+      <form onSubmit={addContact}>
         <div>
           name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          Numbers: <input value={newNumber} onChange={handleNumberChange}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-        {persons.map((name)=><div key={name.name}>{name.name}</div>)}
+        {persons.map((name)=><div key={name.number}>{name.name} {name.number}</div>)}
     </div>
   )
 }
