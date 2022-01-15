479b23e1492ee7cfb47b749d36669c0a3a3f3188
Gary Arzumanyan <garyarzumanyan@gmail.com>
2022-01-14 22:15:46 -0800

    completed ex 2.11

--- Part2/phonebook/src/index.js
+++ Part2/phonebook/src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import axios from 'axios'
 
-ReactDOM.render(
+/* ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
+); */
+
+ReactDOM.render(<App />, document.getElementById('root'))
+
